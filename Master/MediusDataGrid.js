fa83abcf4a2374f38ba402196132f57177dd3ddc
Emil W <emil@fornby.com>
2013-05-02 14:20:54 +0200

    Update in Grid to allow custom data

--- Master/MediusDataGrid.js
+++ Master/MediusDataGrid.js
@@ -1,17 +1,117 @@
 var MediusDataGrid = function(){
 
 	var self = this;
+	
+	var Lib = require("medius/lib/path");
+	
+	/**
+    * Create a manual grid - If custom data is needed to be inserted, this can be done through this Grid
+    *
+    * @params elementName the element to bind the grid to
+    * @params data an array with data objects
+	* @params columns a list of columns where each columns looks like this { ColumnName: record, ValueSource: record }
+	* @params options the option parameter where things can be adjusted to get a specific behaviour
+    * @returns void
+	* 
+	* @example
+    * var grid = new MediusDataGrid();
+	* var proxyData = new MediusData();
+	* var data = proxyData.QueryDocument("Medius.Core.Entities.Help.HelpRequest", "", 10, 1);
+	* var cols = "Id,Title".split(",");//self.gadget.Settings()[2].Value().split(",");
+	* var columns = [];
+	* _.each(cols, function (record) {
+	*			columns.push({ ColumnName: record, ValueSource: record });
+	*		});
+	* var option = {};
+	* grid.CreateManualGrid("#g-@Model.Id", data, columns, option);
+    */
+	self.CreateManualGrid = function(elementName, data, columns, option) {
 		
+		var DataGrid = require("medius/components/grid/dataGrid");
+		var DataSource = require("medius/components/grid/dataSource/default");
+		
+		var gridHelpers = medius.Grid;
+		var dataSource = new DataSource();
+		
+		if(!option) {
+			option = {};
+		};
+		
+		dataSource.Columns(columns);
+		
+		_.each(data, function (record) {
+					//columns.push({ ColumnName: record.replace('.', '_'), ValueSource: record.replace('.', '_') });
+					//record.Columns = ["Id", "Title"];
+					record.isSelected = false;
+					record.Columns = gridHelpers.resolveColumns(record, dataSource.Columns());
+		});
+		
+		dataSource.Rows(data);
+		
+		
+		dataSource.getTotalRows = function (dataQuery) {
+			return this.Rows().length;
+        };
+		
+		dataSource.load = function(dataQuery) {
+			return this.Rows();
+		};
+		if(!option.gridOption) {
+			option.gridOption = {};
+			option.gridOption.search = false;
+			option.gridOption.sort = false;
+			option.gridOption.paging = false;
+		};
+		
+		option.gridOption = self.setDataNavigationLink(option.gridOption, option.linkProperty, true);
+		
+		var dataGrid = new DataGrid(dataSource, option.gridOption);
+
+		
+		self.render(elementName, dataGrid);
+		
+	};
+	/**
+    * Create automatic grid - Handles the common scenario for a data type
+	* The grid itself maintaince the data fetching, sorting, filtering etc.
+    *
+    * @params elementName the element to bind the grid to
+    * @params entityType a string representing the data object type
+	* @params query the query that is applied on the entityType
+	* @params columns a list of columns where each columns looks like this { ColumnName: record, ValueSource: record }
+	* @params options the option parameter where things can be adjusted to get a specific behaviour
+    * @returns void
+	* 
+	* @example
+    * var grid = new MediusDataGrid();
+	* Get the type
+	* var tag = "Medius.Core.Entities.Help.HelpRequest";
+	* //Get the where statement
+	* var whereStatement = "RequestDate > '2011-12-01'";
+	* var cols = ["Id", "Title"];
+	* var pageSize = 10;
+	* var columns = [];
+	* _.each(cols, function (record) {
+	*				columns.push({ ColumnName: record, ValueSource: record });
+	*				});
+	*			
+	*			var option = {};
+	*			option.documentLinkProperty = "Id";
+	*			option.entityGridOption = {};
+	*			option.entityGridOption.pageSize = pageSize;
+	*							
+	*			grid.CreateGrid("#g-@Model.Id", tag, whereStatement, columns, option);
+    */	
 	self.CreateGrid = function(elementName, entityType, query, columns, option){
 	
 		var EntityGrid = require("medius/components/grid/entityGrid");
 		var SyncDataSource = require("medius/components/grid/dataSource/sync");
 		var sync = require("medius/core/sync");
-		var Lib = require("medius/lib/path");
 		
 		if(!option){
 			option = {};
 		}
+		
 		var source = new SyncDataSource();
 		source.Columns(columns);
 		
@@ -58,20 +158,33 @@ var MediusDataGrid = function(){
 		
 		option.entityGridOption.dataSource = source;
 		
-		option.entityGridOption.onClickRow = function(e, y){
-			if(option.documentLinkProperty){
-				window.location = Lib.getBasePath() + "#Tasks/ShowDocument/" + e[option.documentLinkProperty];
-			}
-		};
-		
+		option.entityGridOption = self.setDataNavigationLink(option.entityGridOption, option.documentLinkProperty, true);	
 
 		var entityGrid = new EntityGrid(entityType, option.entityGridOption);
 		
-		entityGrid.setTargetElement(elementName);
-		entityGrid.render();
+		self.render(elementName, entityGrid);
+	};
+	
+	self.setDataNavigationLink = function(gridOption, linkProperty, isDocument) {
+		
+		if(linkProperty){
+				gridOption.onClickRow = function(e, y){
+					window.location = Lib.getBasePath() + "#Tasks/ShowDocument/" + e[linkProperty];
+				};
+		};
+		
+		return gridOption;
+	};
+	
+	self.render = function(elementName, grid) {
+		grid.setTargetElement(elementName);
+		grid.render();
 	};
 };
-
+/**
+    * Create automatic grid - Handles the common scenario for a data type
+	* Shold be deleted from this file when the platform have been updated, require(HQLBuilder...) should be used instead.
+    */	
 var HqlQueryBuilder = function (entityDataQuery) {
 			var self = this,
 				query = "";
